512596ec7c5368e31c67a086199d7f362b6d5741
Nicolas Benoit <123339751+Nikandro1@users.noreply.github.com>
2025-11-16 15:29:31 -0300

    solucionando problema de ruta para imagen de fondo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div 
       className="w-[calc(100%)] h-[calc(100vh)] bg-cover bg-center bg-no-repeat pt-5"
       style={{ 
-        backgroundImage: "fondo-actual.png",
+        backgroundImage: 'url("/fondo-actual.png")',
         backgroundColor: '#ffffffff',
         backgroundSize: '70%'
       }}
