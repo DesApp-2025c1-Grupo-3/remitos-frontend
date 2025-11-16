88d1c577489c423862fd086c5b94985a3ff9fd5d
Nicolas Benoit <123339751+Nikandro1@users.noreply.github.com>
2025-11-16 15:24:24 -0300

    modificacion de fondo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div 
       className="w-[calc(100%)] h-[calc(100vh)] bg-cover bg-center bg-no-repeat pt-5"
       style={{ 
-        backgroundImage: "url('/public/fondo-actual.png')",
+        backgroundImage: "fondo-actual.png",
         backgroundColor: '#ffffffff',
         backgroundSize: '70%'
       }}
